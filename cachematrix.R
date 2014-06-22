ca26a9fd157f1e2cc3fffad7d962b20cf8bae901
Joel W <joeljw@gmail.com>
2014-06-22 18:09:10 -0400

    Yay it works

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Works best if you create a matrix and test the function on it
+## Example: z <- matrix(rnorm(4*4), 4, 4)
 
-## Write a short comment describing this function
+## this function calls the cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-    cacheSolve(x)
+    if(!is.null(i)){
+        cacheSolve(x)
+    }
+    i <<- NULL
 }
 
 
 ## This function will return either the cached matrix or its calcuated inverse
-
 cacheSolve <- function(x) {
-    ## Return a matrix that is the inverse of 'x'
+    # check to see if the matrix is cached
     if(!is.null(i)) {
         message("getting cached data")
         return(i)
     }
+    ## Return a matrix that is the inverse of 'x'
     i <<- solve(x)
     i
 }
-
