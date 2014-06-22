e7f1b97296143637ede3a6105e08420f9b152c46
Joel W <joeljw@gmail.com>
2014-06-22 17:57:22 -0400

    Yay it works

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cacheSolve(x)
 }
 
 
-## Write a short comment describing this function
+## This function will return either the cached matrix or its calcuated inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    ## Return a matrix that is the inverse of 'x'
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    i <<- solve(x)
+    i
 }
+
